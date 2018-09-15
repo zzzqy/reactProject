8783645317fd9c3a013b278185e86eeb0d7541e9
zzzqy <297948088@qq.com>
2018-09-15 08:35:31 +0800

    项目初始化

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Home from './pages/home/index'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <Home></Home>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/pages/home/index.js
@@ -0,0 +1,127 @@
+import React, {Component} from 'react'
+import { TabBar } from 'antd-mobile';
+class Home extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          selectedTab: 'redTab',
+          hidden: false,
+          fullScreen: false,
+        };
+      }
+    render(){
+        return( 
+            <TabBar
+                unselectedTintColor="#949494"
+                tintColor="#33A3F4"
+                barTintColor="white"
+                hidden={this.state.hidden}
+            >
+            <TabBar.Item
+                    title="Life"
+                    key="Life"
+                    icon={<div style={{
+                    width: '22px',
+                    height: '22px',
+                    background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}
+                    />
+                    }
+                    selectedIcon={<div style={{
+                    width: '22px',
+                    height: '22px',
+                    background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}
+                    />
+                    }
+                    selected={this.state.selectedTab === 'Life'}
+                    badge={1}
+                    onPress={() => {
+                    this.setState({
+                        selectedTab: 'Life',
+                    });
+                    }}
+                    data-seed="logId"
+                >
+                    {/* {this.renderContent('Life')} */}
+                </TabBar.Item>
+                <TabBar.Item
+                    title="Hot"
+                    key="Hot"
+                    icon={<div style={{
+                    width: '22px',
+                    height: '22px',
+                    background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}
+                    />
+                    }
+                    selectedIcon={<div style={{
+                    width: '22px',
+                    height: '22px',
+                    background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}
+                    />
+                    }
+                    selected={this.state.selectedTab === 'Hot'}
+                    badge={1}
+                    onPress={() => {
+                    this.setState({
+                        selectedTab: 'Hot',
+                    });
+                    }}
+                    data-seed="logId"
+                >
+                    {/* {this.renderContent('Life')} */}
+                </TabBar.Item>
+                <TabBar.Item
+                    title="Search"
+                    key="Search"
+                    icon={<div style={{
+                    width: '22px',
+                    height: '22px',
+                    background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}
+                    />
+                    }
+                    selectedIcon={<div style={{
+                    width: '22px',
+                    height: '22px',
+                    background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}
+                    />
+                    }
+                    selected={this.state.selectedTab === 'Search'}
+                    onPress={() => {
+                    this.setState({
+                        selectedTab: 'Search',
+                    });
+                    }}
+                    data-seed="logId"
+                >
+                    {/* {this.renderContent('Life')} */}
+                </TabBar.Item>
+                <TabBar.Item
+                    title="Mine"
+                    key="Mine"
+                    icon={<div style={{
+                    width: '22px',
+                    height: '22px',
+                    background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}
+                    />
+                    }
+                    selectedIcon={<div style={{
+                    width: '22px',
+                    height: '22px',
+                    background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}
+                    />
+                    }
+                    selected={this.state.selectedTab === 'Mine'}
+                    badge={1}
+                    onPress={() => {
+                    this.setState({
+                        selectedTab: 'Mine',
+                    });
+                    }}
+                    data-seed="logId"
+                >
+                {/* {this.renderContent('Life')} */}
+                </TabBar.Item>    
+            </TabBar>
+        )
+    }
+}
+export default Home
