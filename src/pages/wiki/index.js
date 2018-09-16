8b83b7ade86ae356c743132c8d896a6efd0ba6f8
zzzqy <297948088@qq.com>
2018-09-16 22:45:04 +0800

    init

---
+++ src/pages/wiki/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+                                
+class Wiki extends Component {
+    render() {
+        return (
+            <div>
+                <input type="text" onClick={
+                    () => {
+                          
+                    }
+                }/>
+            </div>                  
+        );
+    }
+}
+                                
+export default Wiki;
