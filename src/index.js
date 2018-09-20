6238dbbb8a05b71a794252116c106732d7f1cab9
zhm1ng <913418614@qq.com>
2018-09-20 10:39:50 +0800

    wo shi zheng mning

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './style/usage/app.scss'
 import registerServiceWorker from './registerServiceWorker';
-
+// zhengming
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
